3e66c770744acf995d983faedd5a7c837e222d42
musa108 <81855645+musa108@users.noreply.github.com>
2021-04-04 18:59:10 +0100

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,28 @@
+name = "MUSA UMARU"
+department = "COMPUTER SCIENCE"
+print(name)
+print(department)
+
+scare = "boo"
+print (scare)
+
+a = 5
+b = 4
+print(a+b)
+c = 9
+print(c)
+
+
+
+division = 12 / .5
+print(division)
+
+x = 2+5
+print(x)
+
+z = 8%3
+print(z)
+
+a = 2*4
+b = 4+2
+print(a*b)
